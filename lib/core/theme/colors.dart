295e6a93b11ac9f8d24a56a1ac8aa079da928a1f
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-25 15:49:06 +0300

    create helper=>extintion

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class ColorsManeger {
+  static const Color primaryColor = Color(0xFF247cff);
+  static const Color grayColor = Color(0xff757575);
+  static const Color accentColor = Color(0xfff50057);
+}
--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,10 +1,30 @@
+import 'package:doc_doc/core/routing/app_router.dart';
+import 'package:doc_doc/core/routing/routes.dart';
+import 'package:doc_doc/core/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
-  const DocApp({super.key});
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
 
   @override
   Widget build(BuildContext context) {
-    return;
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManeger.primaryColor,
+          fontFamily: 'Poppins',
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBordingScreen,
+        onGenerateRoute: appRouter.genrateRoute,
+      ),
+    );
   }
 }
