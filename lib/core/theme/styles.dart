970333d4dd3576cfa215248a3b6edd196db62d4e
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-26 22:37:29 +0300

    craete doclogoa nd name

---
+++ lib/core/theme/styles.dart
@@ -0,0 +1,16 @@
+import 'package:doc_doc/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles {
+  static TextStyle font24Black700W = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static TextStyle font32BlueBold = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w700,
+    color: ColorsManeger.primaryColor,
+  );
+}
--- lib/featured/onBoarding/onboardong_screen.dart
+++ lib/featured/onBoarding/onboardong_screen.dart
@@ -1,13 +1,36 @@
+import 'package:doc_doc/core/theme/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: SingleChildScrollView(child: Column(children: [
-          
-        ],
-      )));
+    return Scaffold(
+      body: SafeArea(
+        child: (SingleChildScrollView(
+          padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+          child: Column(children: [DocLogoAndName(), SizedBox(height: 30.h)]),
+        )),
+      ),
+    );
+  }
+}
+
+class DocLogoAndName extends StatelessWidget {
+  const DocLogoAndName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset("assets/svgs/docdoc_logo.svg"),
+        SizedBox(width: 10.w),
+        Text("DocDoc", style: TextStyles.font24Black700W),
+      ],
+    );
   }
 }
